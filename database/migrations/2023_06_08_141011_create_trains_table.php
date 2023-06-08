d76bb9d85ae6bfbfc35006913a0e886f439a2741
GiacomoArena <giacomoarena28@gmail.com>
2023-06-08 17:03:37 +0200

    update trains table db

--- database/migrations/2023_06_08_141011_create_trains_table.php
+++ database/migrations/2023_06_08_141011_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('start_station');
+            $table->string('end_station');
+            $table->timestamp('start_time');
+            $table->timestamp('arrival_time');
+            $table->string('train_code',20);
+            $table->tinyInteger('carriages_number')->default(4);
+            $table->boolean('on_time');
+            $table->boolean('deleted');
             $table->timestamps();
         });
     }
